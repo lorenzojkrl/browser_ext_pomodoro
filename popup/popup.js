4409214b56414da9c95372c2eaadb9a5a53f60b7
Lorenzo <lorenzozar@gmail.com>
2022-10-06 08:29:21 +0300

    add and delete tasks

--- popup/popup.js
+++ popup/popup.js
@@ -3,25 +3,22 @@ const tasks = []
 const addTaskBtn = document.getElementById("add-task-btn")
 addTaskBtn.addEventListener("click", () => addTask())
 
-const addTask = () => {
-  const tasksNumber = tasks.length
-  tasks.push("")
-
+const renderTask = (taskNumber) => {
   const taskRow = document.createElement("div")
 
   const text = document.createElement("input")
   text.type = "text"
   text.placeholder = "Enter task..."
+  text.value = tasks[taskNumber]
   text.addEventListener("change", () => {
-    tasks[tasksNumber] = text.value
+    tasks[taskNumber] = text.value
   })
 
   const deleteBtn = document.createElement("input")
   deleteBtn.type = "button"
   deleteBtn.value = "x"
   deleteBtn.addEventListener("click", () => {
-    tasks.splice(taskNum, 1)
-    // Add logic to remove DOM element
+    deleteTask(taskNumber)
   })
 
   taskRow.appendChild(text)
@@ -29,4 +26,23 @@ const addTask = () => {
 
   const taskContainer = document.getElementById("task-container")
   taskContainer.appendChild(taskRow)
+}
+
+const addTask = () => {
+  const taskNumber = tasks.length
+  tasks.push("")
+  renderTask(taskNumber)
+}
+
+const deleteTask = (taskNumber) => {
+  tasks.splice(taskNumber, 1)
+  renderTasks()
+}
+
+const renderTasks = () => {
+  const taskContainer = document.getElementById("task-container")
+  taskContainer.textContent = ""
+  tasks.forEach((taskText, taskNumber) => {
+    renderTask(taskNumber)
+  })
 }
